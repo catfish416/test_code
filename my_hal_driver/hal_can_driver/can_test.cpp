129e17ce04fd93f0b6699f9271c98f3369ee46e8
catfish <catfish416@126.com>
2019-11-06 12:48:27 +0800

    OverView :add my_hal_driver
    Feature : NA
    Bug : NA
    Description : NA

---
+++ my_hal_driver/hal_can_driver/can_test.cpp
@@ -0,0 +1,81 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include "hal_can.h"
+#include "resue.h"
+#include <time.h>
+//#include <linux/can.h>
+//#include <linux/can/raw.h>
+
+
+extern int s_can[2];
+
+#define BUILD_CAN_WRITE_TEST    1
+#define BUILD_CAN_READ_TEST     0
+
+int main(void)
+{
+#if BUILD_CAN_WRITE_TEST
+    Initsocketcan("can0");
+
+    uint8 data[6];
+    for(int i = 0; i < 6; i++)
+    {
+        data[i] = i + 1;
+    }
+
+    struct can_frame write_frame;
+    write_frame.can_id = 0x444;
+    write_frame.can_dlc = 8;
+    memcpy(&write_frame.data[0], &data[0], 6);
+
+    for (int i = 0; i < 1000; i++)
+    {
+        int write_byte = write_canFrame(s_can[0], &write_frame, sizeof(write_frame));
+        if (write_byte != sizeof(write_frame))
+            printf("write can0 error, write_byte=%d\n", write_byte);
+        else
+            printf("write can0 ok\n");
+        //usleep(1000*100);
+        sleep(1);
+    }
+    close(s_can[0]);
+#endif
+
+#if BUILD_CAN_READ_TEST
+    Initsocketcan("can0");
+
+    struct can_frame read_frame;
+    (void)memset(&read_frame, 0, sizeof(read_frame));
+
+    // filter can_id
+    struct can_filter rfilter;
+    rfilter.can_id = 0x07e1;
+    rfilter.can_mask = CAN_SFF_MASK;
+    //setsockopt(s_can[0], SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter));
+
+    int read_nbytes = 0;
+
+    //while((read_nbytes = read_canFrame(s_can[0], &read_frame, sizeof(read_frame)))>0)
+        read_nbytes = read_canFrame(s_can[0], &read_frame, sizeof(read_frame));
+    {
+        struct timespec ti;
+        clock_gettime(CLOCK_MONOTONIC, &ti);
+        uint32_t recvtime = ti.tv_sec * 1000 + ti.tv_nsec / 1000000;
+        printf("time: %d, ID = %x  DLC = %x  %x %x %x %x %x %x %x %x\n",
+            recvtime, read_frame.can_id, read_frame.can_dlc, \
+            read_frame.data[0], read_frame.data[1], read_frame.data[2], read_frame.data[3], \
+            read_frame.data[4], read_frame.data[5], read_frame.data[6],read_frame.data[7]);
+    }
+    printf("read can0 over\n");
+    close(s_can[0]);
+#endif
+
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif /* end of __cplusplus */
---
+++ my_hal_driver/hal_spi_driver/spidev_test.c
@@ -0,0 +1,457 @@
+/*
+ * SPI testing utility (using spidev driver)
+ *
+ * Copyright (c) 2007  MontaVista Software, Inc.
+ * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
+ */
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#define MAX_DATA_LEN        (20)
+#define SPI_SHIFT_TIMES     (8)
+
+#define ADU_DEBUG_ON        1
+#define PRESS_TEST_ON       0
+
+
+static void pabort(const char *s)
+{
+	perror(s);
+	abort();
+}
+
+static const char *device = "/dev/spidev1.1";
+static uint32_t mode;
+static uint8_t bits = 8;
+static uint32_t speed = 500000;
+static uint16_t delay = 2;
+static int verbose;
+
+uint8_t CRC8_table[256]={
+	0x00,0x31,0x62,0x53,0xc4,0xf5,0xa6,0x97,0xb9,0x88,0xdb,0xea,0x7d,0x4c,0x1f,0x2e, \
+	0x43,0x72,0x21,0x10,0x87,0xb6,0xe5,0xd4,0xfa,0xcb,0x98,0xa9,0x3e,0x0f,0x5c,0x6d, \
+	0x86,0xb7,0xe4,0xd5,0x42,0x73,0x20,0x11,0x3f,0x0e,0x5d,0x6c,0xfb,0xca,0x99,0xa8, \
+	0xc5,0xf4,0xa7,0x96,0x01,0x30,0x63,0x52,0x7c,0x4d,0x1e,0x2f,0xb8,0x89,0xda,0xeb, \
+	0x3d,0x0c,0x5f,0x6e,0xf9,0xc8,0x9b,0xaa,0x84,0xb5,0xe6,0xd7,0x40,0x71,0x22,0x13, \
+	0x7e,0x4f,0x1c,0x2d,0xba,0x8b,0xd8,0xe9,0xc7,0xf6,0xa5,0x94,0x03,0x32,0x61,0x50, \
+	0xbb,0x8a,0xd9,0xe8,0x7f,0x4e,0x1d,0x2c,0x02,0x33,0x60,0x51,0xc6,0xf7,0xa4,0x95, \
+	0xf8,0xc9,0x9a,0xab,0x3c,0x0d,0x5e,0x6f,0x41,0x70,0x23,0x12,0x85,0xb4,0xe7,0xd6, \
+	0x7a,0x4b,0x18,0x29,0xbe,0x8f,0xdc,0xed,0xc3,0xf2,0xa1,0x90,0x07,0x36,0x65,0x54, \
+	0x39,0x08,0x5b,0x6a,0xfd,0xcc,0x9f,0xae,0x80,0xb1,0xe2,0xd3,0x44,0x75,0x26,0x17, \
+	0xfc,0xcd,0x9e,0xaf,0x38,0x09,0x5a,0x6b,0x45,0x74,0x27,0x16,0x81,0xb0,0xe3,0xd2, \
+	0xbf,0x8e,0xdd,0xec,0x7b,0x4a,0x19,0x28,0x06,0x37,0x64,0x55,0xc2,0xf3,0xa0,0x91, \
+	0x47,0x76,0x25,0x14,0x83,0xb2,0xe1,0xd0,0xfe,0xcf,0x9c,0xad,0x3a,0x0b,0x58,0x69, \
+	0x04,0x35,0x66,0x57,0xc0,0xf1,0xa2,0x93,0xbd,0x8c,0xdf,0xee,0x79,0x48,0x1b,0x2a, \
+	0xc1,0xf0,0xa3,0x92,0x05,0x34,0x67,0x56,0x78,0x49,0x1a,0x2b,0xbc,0x8d,0xde,0xef, \
+	0x82,0xb3,0xe0,0xd1,0x46,0x77,0x24,0x15,0x3b,0x0a,0x59,0x68,0xff,0xce,0x9d,0xac, \
+};
+
+uint8_t default_tx[] = {
+    0x01, 0x02, 0x03, 0x04, 0x05,
+    0x06, 0x07, 0x08, 0x09, 0x0a,
+    0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14,
+#if 0
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0x40, 0x00, 0x00, 0x00, 0x00, 0x95,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xF0, 0x0D,
+#endif
+};
+
+uint8_t default_rx[ARRAY_SIZE(default_tx)] = {0, };
+char *input_tx;
+
+uint8_t CRC8_function(const uint8_t *p, char counter)
+{
+    uint8_t crc8 = 0;
+
+    for( ; counter > 0; counter--){
+        crc8 = CRC8_table[crc8^*p];
+        p++;
+    }
+    return(crc8);
+}
+
+static void hex_dump(const void *src, size_t length, size_t line_size, char *prefix)
+{
+	int i = 0;
+	const unsigned char *address = src;
+	const unsigned char *line = address;
+	unsigned char c;
+
+	printf("%s | ", prefix);
+	while (length-- > 0) {
+		printf("%02X ", *address++);
+		if (!(++i % line_size) || (length == 0 && i % line_size)) {
+			if (length == 0) {
+				while (i++ % line_size)
+					printf("__ ");
+			}
+			printf(" | ");  /* right close */
+			while (line < address) {
+				c = *line++;
+				printf("%c", (c < 33 || c == 255) ? 0x2E : c);
+			}
+			printf("\n");
+			if (length > 0)
+				printf("%s | ", prefix);
+		}
+	}
+}
+
+/*
+ *  Unescape - process hexadecimal escape character
+ *      converts shell input "\x23" -> 0x23
+ */
+static int unescape(char *_dst, char *_src, size_t len)
+{
+	int ret = 0;
+	char *src = _src;
+	char *dst = _dst;
+	unsigned int ch;
+
+	while (*src) {
+		if (*src == '\\' && *(src+1) == 'x') {
+			sscanf(src + 2, "%2x", &ch);
+			src += 4;
+			*dst++ = (unsigned char)ch;
+		} else {
+			*dst++ = *src++;
+		}
+		ret++;
+	}
+	return ret;
+}
+
+int handle_recv_SPI_msg(uint8_t const *pInput, int input_len, uint8_t *pOutput, int output_len)
+{
+    int ret = -1;
+    if (output_len < MAX_DATA_LEN - 1 || input_len > output_len)
+    {
+        printf("recv_SPI_msg: output_len err");
+        return ret;
+    }
+
+    uint8_t rcv_msg[MAX_DATA_LEN] = {0};
+    uint8_t shift_rcv_msg[SPI_SHIFT_TIMES][MAX_DATA_LEN - 1] = {0};
+
+    printf("SPI receive ...\n");
+    if(0 == CRC8_function(&pInput[0], MAX_DATA_LEN - 1))
+    {
+        printf("recv_SPI_data success\n");
+        memcpy(pOutput, &pInput[0], MAX_DATA_LEN - 1);
+        ret = 0;
+    }
+    else
+    {
+        // for debug receive
+        memcpy(&rcv_msg[0], pInput, input_len);
+        int j = 0;
+        for(j = 0; j < MAX_DATA_LEN; j++)
+        {
+            printf("rcv[%d]: 0x%x \n", j, rcv_msg[j]);
+        }
+        printf("\n");
+        for(j = 0; j < SPI_SHIFT_TIMES; j++)
+        {
+            uint8_t temp1 =(0xFF<<(j+1));
+            uint8_t temp3 = temp1 >>(j+1);
+            uint8_t temp2 = 0xFF <<(SPI_SHIFT_TIMES - 1 - j);
+            //  printf("temp3 = %x temp2= %x\n", temp3,temp2);
+            int n = 0;
+            for(n = 0; n < MAX_DATA_LEN - 1; n++)
+            {
+                shift_rcv_msg[j][n] = ((rcv_msg[n] &temp3)<<(j+1)) |(( rcv_msg[n+1] &temp2)>>(7-j));
+                  printf("0x%x  ,",shift_rcv_msg[j][n]);
+            }
+            printf("\n");
+            if(0 == CRC8_function(&shift_rcv_msg[j][0], MAX_DATA_LEN - 1) 
+                && 0xcc == shift_rcv_msg[j][3] && 0xcc == shift_rcv_msg[j][5])
+            {
+                printf("recv_SPI_data shift success\n");
+                memcpy(pOutput, &shift_rcv_msg[j][0], MAX_DATA_LEN - 1);
+                ret = 0;
+                break;
+            }
+        }
+    }
+
+    if (ret)
+        printf("CRC failed!\n");
+    else {
+        //hex_dump(pOutput, MAX_DATA_LEN, MAX_DATA_LEN, "RX");
+        int k = 0;
+        for (k = 0; k < output_len; k++) {
+            printf("output[%d]: 0x%x \n", k, pOutput[k]);
+        }
+    }
+    printf("\n");
+
+    return ret;
+}
+
+static void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
+{
+	int ret;
+
+	struct spi_ioc_transfer tr = {
+		.tx_buf = (unsigned long)tx,
+		.rx_buf = (unsigned long)rx,
+		.len = len,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+	};
+
+	if (mode & SPI_TX_QUAD)
+		tr.tx_nbits = 4;
+	else if (mode & SPI_TX_DUAL)
+		tr.tx_nbits = 2;
+	if (mode & SPI_RX_QUAD)
+		tr.rx_nbits = 4;
+	else if (mode & SPI_RX_DUAL)
+		tr.rx_nbits = 2;
+	if (!(mode & SPI_LOOP)) {
+		if (mode & (SPI_TX_QUAD | SPI_TX_DUAL))
+			tr.rx_buf = 0;
+		else if (mode & (SPI_RX_QUAD | SPI_RX_DUAL))
+			tr.tx_buf = 0;
+	}
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+		pabort("can't send spi message");
+
+	if (verbose) {
+		// hex_dump(tx, len, 32, "TX");
+        hex_dump(tx, len, len, "TX");
+       }
+    // hex_dump(rx, len, 32, "RX");
+    hex_dump(rx, len, len, "RX");
+
+    uint8_t rcv[20] = {0};
+    handle_recv_SPI_msg(rx, len, rcv, sizeof(rcv));
+}
+
+static void print_usage(const char *prog)
+{
+	printf("Usage: %s [-DsbdlHOLC3]\n", prog);
+	puts("  -D --device   device to use (default /dev/spidev1.1)\n"
+	     "  -s --speed    max speed (Hz)\n"
+	     "  -d --delay    delay (usec)\n"
+	     "  -b --bpw      bits per word \n"
+	     "  -l --loop     loopback\n"
+	     "  -H --cpha     clock phase\n"
+	     "  -O --cpol     clock polarity\n"
+	     "  -L --lsb      least significant bit first\n"
+	     "  -C --cs-high  chip select active high\n"
+	     "  -3 --3wire    SI/SO signals shared\n"
+	     "  -v --verbose  Verbose (show tx buffer)\n"
+	     "  -p            Send data (e.g. \"1234\\xde\\xad\")\n"
+	     "  -N --no-cs    no chip select\n"
+	     "  -R --ready    slave pulls low to pause\n"
+	     "  -2 --dual     dual transfer\n"
+	     "  -4 --quad     quad transfer\n");
+	exit(1);
+}
+
+static void parse_opts(int argc, char *argv[])
+{
+	while (1) {
+		static const struct option lopts[] = {
+			{ "device",  1, 0, 'D' },
+			{ "speed",   1, 0, 's' },
+			{ "delay",   1, 0, 'd' },
+			{ "bpw",     1, 0, 'b' },
+			{ "loop",    0, 0, 'l' },
+			{ "cpha",    0, 0, 'H' },
+			{ "cpol",    0, 0, 'O' },
+			{ "lsb",     0, 0, 'L' },
+			{ "cs-high", 0, 0, 'C' },
+			{ "3wire",   0, 0, '3' },
+			{ "no-cs",   0, 0, 'N' },
+			{ "ready",   0, 0, 'R' },
+			{ "dual",    0, 0, '2' },
+			{ "verbose", 0, 0, 'v' },
+			{ "quad",    0, 0, '4' },
+			{ NULL, 0, 0, 0 },
+		};
+		int c;
+
+		c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NR24p:v", lopts, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'D':
+			device = optarg;
+			break;
+		case 's':
+			speed = atoi(optarg);
+			break;
+		case 'd':
+			delay = atoi(optarg);
+			break;
+		case 'b':
+			bits = atoi(optarg);
+			break;
+		case 'l':
+			mode |= SPI_LOOP;
+			break;
+		case 'H':
+			mode |= SPI_CPHA;
+			break;
+		case 'O':
+			mode |= SPI_CPOL;
+			break;
+		case 'L':
+			mode |= SPI_LSB_FIRST;
+			break;
+		case 'C':
+			mode |= SPI_CS_HIGH;
+			break;
+		case '3':
+			mode |= SPI_3WIRE;
+			break;
+		case 'N':
+			mode |= SPI_NO_CS;
+			break;
+		case 'v':
+			verbose = 1;
+			break;
+		case 'R':
+			mode |= SPI_READY;
+			break;
+		case 'p':
+			input_tx = optarg;
+			break;
+		case '2':
+			mode |= SPI_TX_DUAL;
+			break;
+		case '4':
+			mode |= SPI_TX_QUAD;
+			break;
+		default:
+			print_usage(argv[0]);
+			break;
+		}
+	}
+	if (mode & SPI_LOOP) {
+		if (mode & SPI_TX_DUAL)
+			mode |= SPI_RX_DUAL;
+		if (mode & SPI_TX_QUAD)
+			mode |= SPI_RX_QUAD;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	int fd;
+	uint8_t *tx;
+	uint8_t *rx;
+	int size;
+
+	parse_opts(argc, argv);
+
+	fd = open(device, O_RDWR);
+	if (fd < 0)
+		pabort("can't open device");
+
+	/*
+	 * spi mode
+	 */
+	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
+
+	ret = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
+
+	/*
+	 * bits per word
+	 */
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't set bits per word");
+
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't get bits per word");
+
+	/*
+	 * max speed hz
+	 */
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't set max speed hz");
+
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't get max speed hz");
+
+	printf("spi mode: 0x%x\n", mode);
+	printf("bits per word: %d\n", bits);
+	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+
+    printf("delay: %d usecs\n", delay);
+
+	if (input_tx) {
+		size = strlen(input_tx+1);
+		tx = malloc(size + 2);
+		rx = malloc(size + 2);
+        memset(tx, 0, size + 2);
+
+		size = unescape((char *)tx, input_tx, size);
+
+        tx[size] = CRC8_function(tx, size);
+        tx[size + 1] = 0xAA;
+
+        printf("crc = %d, last = 0x%x, len = %d\n", tx[size], tx[size + 1], size);
+
+#if PRESS_TEST_ON
+        /* loop test begin */
+        int i = 0;
+        for (; i < 100; i++) {
+            //tx[size + 1] = i + 1;
+            tx[4] = i + 1;
+            tx[size] = CRC8_function(tx, size);
+            tx[size + 1] = 0xAA;
+            transfer(fd, tx, rx, size + 2);
+            usleep(300);
+        }
+        /* loop test end */
+#else
+        transfer(fd, tx, rx, size + 2);
+#endif
+		free(rx);
+		free(tx);
+	} else {
+		transfer(fd, default_tx, default_rx, sizeof(default_tx));
+	}
+
+	close(fd);
+
+	return ret;
+}
